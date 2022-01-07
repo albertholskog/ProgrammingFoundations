83a019f410e031a424c37732c5a205d21a475c7d
albertholskog <91052944+albertholskog@users.noreply.github.com>
2022-01-07 10:08:53 +0100

    ferdig

---
+++ js/script.js
@@ -0,0 +1,37 @@
+//Question 1
+
+let number1 = 10;
+const car = "Audi";
+let bigGame = true;
+
+console.log(car);
+
+//Question 2
+
+let firstName = "Albert";
+let lastName = "Holskog";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+//Question 3
+
+let animal = "frog";
+
+console.log("the type of frog is", typeof animal);
+
+//Question 4
+
+let orderHasShipped = true;
+
+if (orderHasShipped === true) {
+  console.log("The order shipped");
+} else if (orderHasShipped === false) {
+  console.log("The order did not ship");
+}
+
+//Question 5
+
+for (var i = 7; i <= 13; i++) {
+  console.log(i);
+}
